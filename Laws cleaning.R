1ff5a8e6d676d1faf42f726308eb9e96a5136594
Rick-Mackenbach <r.p.mackenbach@tilburguniversity.edu>
2018-06-24 17:48:15 +0200

    Improvements
    Open carry and closed carry not used in laws

--- Laws cleaning.R
+++ Laws cleaning.R
@@ -2,27 +2,29 @@
 
 ## Load the dataset
 laws <- read.csv('Laws.csv', stringsAsFactors = FALSE)
+laws$Open.carry.allowed_H <- NULL
+laws$Open.carry.allowed_L <- NULL
 
 ## Change all values to integer scores
 laws[-1][laws[-1] != 'Y'] <- 1
 laws[-1][laws[-1] == 'Y'] <- 0
 
 ## Change the values to integers
-laws[,2:13] <- sapply(laws[,2:13],as.numeric)
+laws[,2:11] <- sapply(laws[,2:11],as.numeric)
 sapply(laws, class)
 
 ## Sum the rows and change column name
-laws[14] <- rowSums(laws[-1])
+laws[12] <- rowSums(laws[-1])
 colnames(laws)[1] <- 'State'
-colnames(laws)[14] <- 'Law Score'
+colnames(laws)[12] <- 'Law Score'
 
 ## Remove dirty dataset
 cleaned_laws <- laws
 rm(laws)
 
 ## Save cleaned dataset
-# write.csv(cleaned_laws, file = "cleaned_laws.csv", row.names = FALSE)
+write.csv(cleaned_laws, file = "cleaned_laws.csv", row.names = FALSE)
 
 ## Save Law score per state
 state_law_scores <- select(cleaned_laws, 'State', 'Law Score')
-#write.csv(state_law_scores, file = "state_law_scores", row.names = FALSE)
+write.csv(state_law_scores, file = "state_law_scores", row.names = FALSE)
